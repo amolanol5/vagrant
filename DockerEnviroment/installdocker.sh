fd2ea0af8d87297d3fa40a29ef582749027e02a8
Andres Molano <andresmolanolujan@gmail.com>
2021-11-04 16:24:41 -0500

    change file to install docker

--- DockerEnviroment/installdocker.sh
+++ DockerEnviroment/installdocker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #instalation docker in vm
 
-yum update -y
-yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum clean all -y
+#yum update -y
+yum install -y yum-utils
 yum install docker-ce docker-ce-cli containerd.io -y
 
 groupadd docker
